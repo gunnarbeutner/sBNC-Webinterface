2d91a1c61b586aa244dadd51099996ef2147138a
Felix Kunsmann <felix@kunsmann.eu>
2010-07-15 12:13:16 +0200

    English language available

---
+++ lang/en.php
@@ -0,0 +1,36 @@
+<?php
+	$LANG = array(
+			'menu_logout' => 'Logout',
+			'menu_index' => 'Index',
+			'menu_password' => 'Change password',
+			'menu_settings' => 'Settings',
+			'menu_traffic' => 'Traffic',
+			'menu_log' => 'Awaylog',
+			
+			'index_welcome' => 'Welcome, %s!',
+			'index_uptime' => 'You are connected to IRC for <strong>%s</strong>.',
+			'index_nick' => 'Your current Nick is <strong>%s</strong>, Awaynick is <strong>%s</strong>.',
+			'index_server' => 'You are connected to Server <strong>%s</strong>.',
+			'index_traffic' => 'Traffic: Incoming %s, Outgoing %s',
+			'index_status' => 'Online-Status',
+			'index_statusimage' => 'You are able to show your Status using the following image: <img src="/status/%s.gif" alt="Online-Status of %s" />',
+			'index_channels' => 'Channels',
+			
+			'settings_awaynick' => 'Awaynick',
+			'settings_awaynick_changed' => 'Awaynick was changed to <strong>%s</strong>.',
+			'settings_away' => 'Away',
+			'settings_away_changed' => 'Away was changed to <strong>%s</strong>.',
+			'settings_nickserv' => 'NickServ',
+			'settings_nick' => 'Nickname',
+			'settings_nick_changed' => 'Nickname was changed to <strong>%s</strong>.',
+			'settings_password' => 'Password',
+			'settings_password_repeat' => 'Repeat password',
+			'settings_password_changed' => 'Password was changed to <strong>%s</strong>.',
+			
+			'log_erased' => 'Log was erased.',
+			'log_prompt' => 'Empty log',
+			
+			'lang_changed' => 'Language changed',
+			'lang_changed_text' => 'Language was changed to <strong>english</strong>.'
+		);
+?>
