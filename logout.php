bc675af44a08cf3ab0a3a08efc48a198aa245e06
Felix Kunsmann <felix@kunsmann.eu>
2010-07-15 12:52:25 +0200

    Additional Pages for logged in Users

---
+++ logout.php
@@ -0,0 +1,8 @@
+<?php
+	require_once 'core/global.php';
+	
+	session_destroy();
+	
+	$_SESSION = array();
+	
+	header('Location: http://'.$_SERVER['HTTP_HOST'].pathinfo($_SERVER['REQUEST_URI'], PATHINFO_DIRNAME).'/');?>
---
+++ user_index.php
@@ -0,0 +1,74 @@
+<?php
+	require_once 'core/global.php';
+	
+	if ($loggedin) {
+		$traffic = $sbnc->call("gettraffic");
+		$nick = $sbnc->call("getvalue", array("nick"));
+		$awaynick = $sbnc->call("getvalue", array("awaynick"));
+		$server = $sbnc->call("getvalue", array("server"));
+		$port = $sbnc->call("getvalue", array("port"));
+				
+				if (empty($awaynick)) {
+					$awaynick = $nick;
+				}
+				
+				if (empty($server)) {
+					$server = '-';
+				} elseif ($port != 6667) {
+					$server .= ':'.$port;
+				}
+				
+				$breadcrumb = array($_SESSION['user'], 'Index');
+				
+				if (isset($_POST['lang']) && file_exists('lang/'.$_POST['lang'].'.php')) {
+					$sbnc->call("setlang", array($_POST['lang']));
+					
+					$_SESSION['language'] = $_POST['lang'];
+					
+					require 'lang/'.$_SESSION['language'].'.php';
+					require_once 'templates/header.html';
+?>
+						<div class="settings saved">
+							<h3><?php echo $LANG['lang_changed']; ?></h3>
+							<p><?php echo $LANG['lang_changed_text']; ?></p>
+						</div>
+<?php
+				} else {
+					require_once 'templates/header.html';
+				}
+?>
+					<h3><?php printf($LANG['index_welcome'], $nick); ?></h3>
+					
+					<p><?php printf($LANG['index_uptime'], Func::duration($sbnc->call("getvalue", array("uptime")))); ?> <?php printf($LANG['index_nick'], $nick, $awaynick); ?> <?php printf($LANG['index_server'], $server); ?></p>
+					<p><?php printf($LANG['index_traffic'], Func::byte_format($traff[2]+$traff[0]), Func::byte_format($traff[3]+$traff[1]) ); ?></p>
+					
+					<h3><?php echo $LANG['index_channels']; ?></h3>
+					<ul>
+<?php
+				foreach($sbnc->call("getchannels") as $channel) {
+?>
+						<li><?php echo $channel; ?></li>
+<?php
+				}
+?>
+					</ul>
+					
+					<form action="/<?php echo $_SESSION['sbnc_user']; ?>/index.html" method="post">
+						<fieldset>
+							<legend>Set Language</legend>
+							
+							<label for="lang">Select Language</label>
+							<select name="lang" id="lang">
+								<option value="de"<?php if ($sbnc->call('getlang') == 'de') { echo ' selected="selected"'; } ?>>Deutsch</option>
+								<option value="en"<?php if ($sbnc->call('getlang') == 'en') { echo ' selected="selected"'; } ?>>English</option>
+							</select>
+							
+							<input type="submit" value="Submit" class="submit" />
+						</fieldset>
+					</form>
+<?php
+		require_once 'templates/footer.html';
+	} else {
+		header('Location: http://'.$_SERVER['HTTP_HOST'].pathinfo($_SERVER['REQUEST_URI'], PATHINFO_DIRNAME).'/', true, 307);
+	}
+?>
