748768d5d685469813331e82c367e5ce94f12c8e
Felix Kunsmann <felix@kunsmann.eu>
2010-07-15 12:13:01 +0200

    Deutsche Sprache verfügbar.

---
+++ lang/de.php
@@ -0,0 +1,36 @@
+<?php
+	$LANG = array(
+			'menu_logout' => 'Abmelden',
+			'menu_index' => 'Übersicht',
+			'menu_password' => 'Passwort ändern',
+			'menu_settings' => 'Einstellungen',
+			'menu_traffic' => 'Traffic',
+			'menu_log' => 'Awaylog',
+			
+			'index_welcome' => 'Willkommen, %s!',
+			'index_uptime' => 'Du bist seit <strong>%s</strong> im IRC unterwegs.',
+			'index_nick' => 'Dein derzeitiger Nickname ist <strong>%s</strong>, dein Awaynick ist <strong>%s</strong>.',
+			'index_server' => 'Du bist zum Server <strong>%s</strong> verbunden.',
+			'index_traffic' => 'Du hast bisher %s eingehenden Traffic und %s ausgehenden Traffic verursacht.',
+			'index_status' => 'Online-Status',
+			'index_statusimage' => 'Um in Foren, Blogs etc. deinen Onlinestatus im IRC anzuzeigen, kannst du folgende Grafik verwenden: <img src="/status/%s.gif" alt="Online-Status von %s" />',
+			'index_channels' => 'Kanäle',
+			
+			'settings_awaynick' => 'Awaynick',
+			'settings_awaynick_changed' => 'Der Awaynick wurde auf <strong>%s</strong> geändert.',
+			'settings_away' => 'Abwesenheitsnachricht',
+			'settings_away_changed' => 'Die Abwesenheitsnachricht wurde auf <strong>%s</strong> geändert.',
+			'settings_nickserv' => 'NickServ',
+			'settings_nick' => 'Nickname',
+			'settings_nick_changed' => 'Der Nickname wurde auf <strong>%s</strong> geändert.',
+			'settings_password' => 'Passwort',
+			'settings_password_repeat' => 'Passwort wiederholen',
+			'settings_password_changed' => 'Das Passwort wurde auf <strong>%s</strong> geändert.',
+			
+			'log_erased' => 'Das Log wurde gelöscht.',
+			'log_prompt' => 'Log leeren',
+			
+			'lang_changed' => 'Sprache geändert',
+			'lang_changed_text' => 'Die Sprache wurde auf <strong>Deutsch</strong> geändert.'
+		);
+?>
